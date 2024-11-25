e75feafeb35600c6f8beda67eccf3115430330c4
Shikher Jain <shikherjain786@gmail.com>
2024-11-25 15:21:58 +0530

    WEB DEV

---
+++ FRONTEND/Forms/action.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        Welcome <?php echo $_POST["name"];?><br>
+        S/O <?php echo $_POST["fname"];?><br>
+        And <?php echo $_POST["mname"];?><br>
+        Email address <?php echo $_POST["email"];?><br>
+    </body>
+</html>
